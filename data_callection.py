c0ed2e07ff80a2dd26cce10dac99cbbaec5963af
DESKTOP-0K70OVM\exeller <elibrch@gmail.com>
2020-12-23 23:06:29 +0200

    uloading data and and data copllection script

--- data_callection.py
+++ data_callection.py
@@ -5,4 +5,6 @@ import pandas as pd
 
 path = "C:\\Users\\exeller\\Documents\\Data Science Projects\\ds_salary_proj\\chromedriver"
 
-df = gs.get_jobs("data scientist", 29, False, path, 2)
+df = gs.get_jobs("data scientist", 1000, False, path, 2)
+
+df.to_csv('glassdoor_jobs.csv',index = False)
